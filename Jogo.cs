b94582a232f57d25132ea08bf9121f112f8c7ea2
elavamosnos <kanxa.tfj@gmail.com>
2021-10-12 15:59:09 -0300

    Adicionando V1 Jogos

--- Jogo.cs
+++ Jogo.cs
@@ -1,20 +1,22 @@
-namespace plataformaJogos
+using System;
+
+namespace sistemaJogo
 {
-    public abstract class Jogo
+public abstract class Jogo
     {
         public string nome { get; set; }
         
         public string idJogo { get; set; }
 
-        public string online { get; set; }
-
-        public string dataLancamento { get; set; }
-
+        public string genero { get; set; } 
+        
         public string resumoJogo { get; set; }
+        
+        public DateTime dataLancamento { get; set; }
 
-        public string personagensPrincipal { get; set; }
+        public bool lojaDisponivel { get; set; }
 
-        public string lojaDisponivel { get; set; }
+        public bool online { get; set; }
 
     }
 
